8b4d054b7b521c8aa194eb6eb7b6524e2cf941b0
krupikivan <krupikivan@gmail.com>
2019-03-21 10:26:37 -0300

    Listados

---
+++ listado/insertListxcsubcate.php
@@ -0,0 +1,63 @@
+<?php
+
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/api/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+//Obtener archivos para la conexion
+include_once 'config/database.php';
+include_once 'objects/listado.php';
+
+//Obtener datos de POST desde un JSON
+$data = json_decode(file_get_contents("php://input"));
+
+//Corroboramos primero que se hayan ingresados datos
+if($data != NULL){
+    //Obtener conexion con DB
+    $database = new Database();
+    $db = $database->getConnection();
+
+    //Instanciar cliente
+    $listado = new Listado($db);
+
+    // Setear valored de listado
+    $listado->fechaCreacion = $data->fechaCreacion;
+    $listado->fechaCobro = $data->fechaCobro;
+    $listado->fechaCompra = $data->fechaCompra;
+    $listado->nombre = $data->nombre;
+    $listado->filas = $data->filas;
+
+    //Creamos el listado
+    if($listado->createName()){
+        $id = $listado->getId();
+        $filas = $listado->getFilas();
+
+        if($listado->createCategory($id, $filas)){
+            // set response code
+            http_response_code(200);
+
+            //Mostramos mensaje
+            echo json_encode(array("message" => "Listado creado!"));
+        }
+        
+    }
+
+    //Mensaje sino se pudo crear
+    else{
+
+        //Seteamos estado
+        http_response_code(400);
+
+        echo json_encode(array("message" => "El listado ya existe"));
+    }
+}
+else{
+    //Seteamos estado
+    http_response_code(400);
+    echo json_encode(array("message" => "Se requieren datos"));
+}
+?>
--- objects/listado.php
+++ objects/listado.php
@@ -5,6 +5,7 @@ class Listado{
     // database connection and table name
     private $conn;
     private $table_name = "listados";
+
  
     // object properties
     public $idListado;
@@ -81,6 +82,137 @@ class Listado{
          
             return $stmt;
         }
+
+//Creamos el listado de compras en la tabla listado
+function createName(){
+    
+    //Existe el listado?
+    if($this->listExist()){
+        return false;
+    }
+
+    //Insertamos query
+	$query = "INSERT INTO
+    " . $this->table_name . "
+    (`idListado`, `fechaCreacion`, `fechaCobro`, `fechaCompra`, `nombre`) VALUES
+    (NULL, '".$this->fechaCreacion."', '".$this->fechaCobro."', '".$this->fechaCompra."', '".$this->nombre."')";
+
+    //Preparamos la query
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $this->fechaCreacion=htmlspecialchars(strip_tags($this->fechaCreacion));
+    $this->fechaCobro=htmlspecialchars(strip_tags($this->fechaCobro));
+    $this->fechaCompra=htmlspecialchars(strip_tags($this->fechaCompra));
+    $this->nombre=htmlspecialchars(strip_tags($this->nombre));
+
+    // bind the values
+    $stmt->bindParam(':fechaCreacion', $this->fechaCreacion);
+    $stmt->bindParam(':fechaCobro', $fechaCobro);
+    $stmt->bindParam(':fechaCompra', $this->fechaCompra);
+    $stmt->bindParam(':nombre', $this->nombre);
+
+
+    //Ejecutamos el script y corroboramos si la query esta OK
+    if($stmt->execute()){
+    
+        $stmt->bindParam(':idListado', $this->idListado);
+    
+        return true;
+    }
+
+    return false;
+    }
+}
+
+function getId(){
+    return $idListado;
+}
+
+    //Existe el nombre de la lista?
+function listExist(){
+ 
+    //Chequear si existe el usuario
+    $query = "SELECT *
+            FROM " . $this->table_name . "
+            WHERE nombre = '".$this->nombre."'";
+ 
+    //Preparamos query
+    $stmt = $this->conn->prepare( $query );
+ 
+    //Sanitizar
+    $this->nombre=htmlspecialchars(strip_tags($this->nombre));
+ 
+    //Enlazamos valores
+    $stmt->bindParam(1, $this->nombre);
+ 
+    //Ejecutamos query
+    $stmt->execute();
+ 
+    //Numero de filas
+    $num = $stmt->rowCount();
+ 
+    // Si existe asignamos valores al objeto (lo podemos usar para manejo de sesiones)
+    if($num>0){
+        
+        // False porque existe en la DB
+        return false;
+    }
+ 
+    // True porque no existe en la DB
+    return true;
+}
+
+//Creamos el listado de compras en la tabla listadoxsubcategorias
+function createCategory($id, $filas){
+
+    //Array to keep track of errors
+	$my_errors = array();
+
+    for($count = 0; $count < sizeof($filas); $count++)
+    {
+        if(!empty($filas[$count]))
+        {
+            $student = mysql_real_escape_string($id[$count]);
+            $mark = mysql_real_escape_string($score[$count]);
+            
+            $query = "INSERT INTO try (id, score) VALUES('".$student."',$mark)";
+            if(!mysql_query($query))
+            {
+                $my_errors[] = $id[$count];
+            }
+        }
+    }
+
+    //Insertamos query
+	$query = "INSERT INTO listadoxsubcategorias
+    (`idListadoxsubcate`, `idListado`, `idCat`) VALUES
+    (NULL, '".$this->fechaCreacion."', '".$this->fechaCobro."', '".$this->fechaCompra."', '".$this->nombre."')";
+
+    //Preparamos la query
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $this->fechaCreacion=htmlspecialchars(strip_tags($this->fechaCreacion));
+    $this->fechaCobro=htmlspecialchars(strip_tags($this->fechaCobro));
+    $this->fechaCompra=htmlspecialchars(strip_tags($this->fechaCompra));
+    $this->nombre=htmlspecialchars(strip_tags($this->nombre));
+
+    // bind the values
+    $stmt->bindParam(':fechaCreacion', $this->fechaCreacion);
+    $stmt->bindParam(':fechaCobro', $fechaCobro);
+    $stmt->bindParam(':fechaCompra', $this->fechaCompra);
+    $stmt->bindParam(':nombre', $this->nombre);
+
+
+    //Ejecutamos el script y corroboramos si la query esta OK
+    if($stmt->execute()){
+    return true;
+    }
+
+    return false;
+    }
+}
 
 }
 ?>
