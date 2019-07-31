71b62320ea4e771e0a303cb6cca08c3cacccb717
krupikivan <krupikivan@gmail.com>
2019-07-31 00:10:07 -0300

    Agregado buscador scanner

--- objects/producto.php
+++ objects/producto.php
@@ -55,5 +55,60 @@ class Producto{
             return $stmt;
         }
 
+        //Le marcamos en 1 la casilla creado de la tabla listado
+function getProductScanned(){
+
+    //Insertamos query
+	$query = "SELECT p.idProducto, p.descripcion, c.descripcion as 'categoria', m.nombre as 'marca', p.precio FROM
+    " . $this->table_name . " p
+    JOIN categorias c on c.idCategoria = p.idCategoriaFK 
+    JOIN marcas m on m.idMarca = p.idMarcaFK
+    WHERE codigo like '".$this->codigo."'";
+
+    //Preparamos la query
+    $stmt = $this->conn->prepare($query);
+
+    // sanitize
+    $this->idProducto=htmlspecialchars(strip_tags($this->idProducto));
+    $this->descripcion=htmlspecialchars(strip_tags($this->descripcion));
+    $this->categoria=htmlspecialchars(strip_tags($this->categoria));
+    $this->marca=htmlspecialchars(strip_tags($this->marca));
+    $this->precio=htmlspecialchars(strip_tags($this->precio));
+
+    // bind the values
+    $stmt->bindParam(':idProducto', $this->idProducto);
+    $stmt->bindParam(':descripcion', $this->descripcion);
+    $stmt->bindParam(':categoria', $this->categoria);
+    $stmt->bindParam(':marca', $this->marca);
+    $stmt->bindParam(':precio', $this->precio);
+
+    //Ejecutamos el script y corroboramos si la query esta OK
+    $stmt->execute();
+
+    //Numero de filas
+    $num = $stmt->rowCount();
+
+    // Si existe asignamos valores al objeto (lo podemos usar para manejo de sesiones)
+    if($num>0){
+
+        // Traemos valores
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        //Asignamos valores
+        $this->idProducto = $row['idProducto'];
+        $this->descripcion = $row['descripcion'];
+        $this->categoria = $row['categoria'];
+        $this->marca = $row['marca'];
+        $this->precio = $row['precio'];
+
+
+        // True porque existe en la DB
+        return $stmt;
+    }
+ 
+    // False porque no existe en la DB
+    return $stmt;
+}
+
 }
 ?>
---
+++ producto/readBarcode.php
@@ -0,0 +1,64 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+ 
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/producto.php';
+ 
+$data = $_GET['codigo'];
+
+//Corroboramos primero que se hayan ingresados datos
+if($data != NULL){
+    //Obtener conexion con DB
+    $database = new Database();
+    $db = $database->getConnection();
+
+    //Instanciar listado
+    $producto = new Producto($db);
+
+    // Setear valored de nombre para buscar en el where
+    $producto->codigo = $data;
+
+    //Ejecutamos
+    $stmt = $producto->getProductScanned();
+
+    //Contamos si hay filas encontradas
+    $num = $stmt->rowCount();
+    $list_arr=array();
+     
+            $list_item=array(
+                "idProducto" => $producto->idProducto,
+                "descripcion" => $producto->descripcion,
+                "categoria" => $producto->categoria,
+                "marca" => $producto->marca,
+                "precio" => $producto->precio,
+            );
+            array_push($list_arr, $list_item);
+
+    // check if more than 0 record found
+    if($num>0){
+    
+        // set response code - 200 OK
+        http_response_code(200);
+    
+        // show products data in json format
+        echo json_encode($list_arr);
+    }
+    else{
+    
+        // set response code - 404 Not found
+        http_response_code(404);
+    
+        // tell the user no products found
+        echo json_encode($list_arr);
+    }
+}
+else{
+    //Seteamos estado
+    http_response_code(400);
+    //Se requieren datos
+    echo json_encode(false);
+}
+?>
