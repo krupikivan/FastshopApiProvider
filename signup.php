8bee66a957a58e05aeb562783de27ef5a79991ef
krupikivan@gmail.com <krupikivan@gmail.com>
2020-11-22 16:39:10 -0300

    test

--- signup.php
+++ signup.php
@@ -2,7 +2,7 @@
 
 // required headers
 // header("Access-Control-Allow-Origin: http://localhost/FastshopApiProvider/");
-header("Access-Control-Allow-Origin: https://aqueous-fjord-12024.herokuapp.com/api/");
+header("Access-Control-Allow-Origin: https://aqueous-fjord-12024.herokuapp.com/");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Max-Age: 3600");
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+echo json_encode(
+    array(
+        "test" => 'Success'
+    )
+);
+
+?>
